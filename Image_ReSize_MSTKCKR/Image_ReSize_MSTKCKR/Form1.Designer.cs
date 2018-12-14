3fe5443e71d7189010181624327eba41d01b82be
MESUT KOÇAKER <mesut0723@gmail.com>
2018-12-15 01:10:42 +0300

    Image_ReSize Resim Boyutlandırma uygulaması
    Umarım işe yarar

---
+++ Image_ReSize_MSTKCKR/Image_ReSize_MSTKCKR/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Image_ReSize_MSTKCKR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.picNewSize = new System.Windows.Forms.PictureBox();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNewSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnSaveAs);
+            this.panel1.Controls.Add(this.btnRun);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.txtHeight);
+            this.panel1.Controls.Add(this.txtWidth);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.picOriginal);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 470);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.Location = new System.Drawing.Point(12, 12);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(175, 138);
+            this.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picOriginal.TabIndex = 1;
+            this.picOriginal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Width  : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Height : ";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtWidth.Location = new System.Drawing.Point(74, 224);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(113, 26);
+            this.txtWidth.TabIndex = 4;
+            this.txtWidth.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtWidth_KeyUp);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHeight.Location = new System.Drawing.Point(74, 267);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(113, 26);
+            this.txtHeight.TabIndex = 5;
+            this.txtHeight.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtWidth_KeyUp);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 311);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto Size";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Enabled = false;
+            this.btnRun.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRun.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(12, 347);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(175, 50);
+            this.btnRun.TabIndex = 7;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // picNewSize
+            // 
+            this.picNewSize.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picNewSize.Location = new System.Drawing.Point(206, 12);
+            this.picNewSize.Name = "picNewSize";
+            this.picNewSize.Size = new System.Drawing.Size(441, 446);
+            this.picNewSize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picNewSize.TabIndex = 2;
+            this.picNewSize.TabStop = false;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Enabled = false;
+            this.btnSaveAs.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnSaveAs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveAs.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAs.Location = new System.Drawing.Point(12, 408);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(175, 50);
+            this.btnSaveAs.TabIndex = 8;
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(659, 470);
+            this.Controls.Add(this.picNewSize);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(675, 465);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMAGE RESIZE MESUT KOÇAKER";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNewSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picNewSize;
+    }
+}
+
---
+++ Image_ReSize_MSTKCKR/Image_ReSize_MSTKCKR/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_ReSize_MSTKCKR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap img;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Image Resize Application and Developer: Mesut Koçaker";
+            file.Filter = "png image|*.png|jpg image|*.jpg|All Files|*.*";
+            #region file.filters
+            /*
+            *.*     All File
+            *.jpg   jpg file
+            *.png   png file
+            *.gif   gif file
+            *.bmp   bmp file
+            *.txt   txt file
+            .
+            .
+            .
+            */
+            #endregion
+            if (file.ShowDialog() == DialogResult.OK)
+            {// if value is ok
+                //  picOriginal.ImageLocation = file.FileName;
+                img = new Bitmap(file.FileName);     // new Bitmap Object
+                txtWidth.Text = img.Width.ToString();       // image width
+                txtHeight.Text = img.Height.ToString();     // image height
+                picOriginal.ImageLocation = file.FileName;  // original image show
+
+                btnRun.Enabled = true;
+            }
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtWidth.Text == txtHeight.Text && txtHeight.Text == "")
+                {//     width and height == ""
+                    btnRun.Enabled = false;
+                    btnSaveAs.Enabled = false;
+                }
+                else
+                {
+                    img = new Bitmap(img, new Size(int.Parse(txtWidth.Text), int.Parse(txtHeight.Text)));
+                    picNewSize.Image = img;
+                    btnSaveAs.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                btnRun.Enabled = false;
+                btnSaveAs.Enabled = false;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtWidth.Text == txtHeight.Text && txtHeight.Text == "")
+                {//     width and height == ""
+                    btnRun.Enabled = false;
+                    btnSaveAs.Enabled = false;
+                }
+                else
+                {
+                    SaveFileDialog saveFile = new SaveFileDialog();
+                    saveFile.Filter = "png image|*.png|jpg image|*.jpg|All Files|*.*";
+                    if (saveFile.ShowDialog() == DialogResult.OK)
+                    {
+                        picNewSize.Image.Save(saveFile.FileName);
+                        MessageBox.Show("Correct Save As");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                autoSize("txtWidth");
+            }
+        }
+
+        private void autoSize(string txtName)
+        {
+            if (txtWidth.Text != "" && txtName==txtWidth.Name)
+            {
+                try
+                {
+                    int width = int.Parse(txtWidth.Text);
+                    int originalWidth = img.Width;
+                    int originalHeight = img.Height;
+
+                    int height = (originalHeight * width) / originalWidth;
+
+                    txtHeight.Text = Math.Abs(height).ToString();
+                }
+                catch
+                {
+                }
+            }
+            else if (txtHeight.Text != "" && txtName == txtHeight.Name)
+            {
+                try
+                {
+                    int height = int.Parse(txtHeight.Text);
+                    int originalWidth = img.Width;
+                    int originalHeight = img.Height;
+
+                    int width = (originalWidth * height) / originalHeight;
+
+                    txtWidth.Text = Math.Abs(width).ToString();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void txtWidth_KeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (checkBox1.Checked)
+            {
+                    autoSize(txt.Name);
+            }
+        }
+    }
+}
